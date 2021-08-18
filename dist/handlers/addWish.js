ab7e5b421cbf9735b530507e872a7566a99b2396
T-Damer <dpancov64@gmail.com>
2021-08-18 22:09:23 +0300

    removed dist, updated locales

--- dist/handlers/addWish.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addWish = void 0;
-// 1. Send a message with questions
-function addWish(ctx) {
-    return ctx.replyWithHTML(ctx.i18n.t('addWish'));
-}
-exports.addWish = addWish;
-//# sourceMappingURL=addWish.js.map
--- dist/handlers/share.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.share = void 0;
-// 1. Send a message with questions
-function share(ctx) {
-    return ctx.replyWithHTML(ctx.i18n.t('share'));
-}
-exports.share = share;
-//# sourceMappingURL=share.js.map
