db6c06490cb2f28d4e537845916374b86967a826
T-Damer <dpancov64@gmail.com>
2021-11-15 18:30:13 +0300

    updated packages and readme

--- src/handlers/addBasket.ts
+++ src/handlers/addBasket.ts
@@ -1,7 +1,13 @@
+const Composer = require('telegraf/composer')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Markup = require('telegraf/markup')
+const WizardScene = require('telegraf/scenes/wizard')
 import { Context } from 'telegraf'
 
 // 1. Send a message with questions
 export function addBasket(ctx: Context) {
+  ctx.state.scene = 'addBasket'
   return ctx.replyWithHTML(ctx.i18n.t('addBasket'))
 }
 // 2. User completes the fields
